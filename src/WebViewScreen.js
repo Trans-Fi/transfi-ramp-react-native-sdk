feae00375974889f4b1e49abab8049d464917486
akash hamse <akashhamse@akashs-MacBook-Air.local>
2023-03-30 11:06:38 +0530

    image changed

--- src/WebViewScreen.js
+++ src/WebViewScreen.js
@@ -1,36 +1,44 @@
 import React from 'react';
 import { View, Image, StyleSheet } from 'react-native';
 import WebView from 'react-native-webview';
-export const logo = require('./assets/logo.jpeg');
+export const logo = require('./assets/logo-ori.gif');
 
 const WebViewScreen = (props) => {
   const Loading = () => {
     return (
-      <View style={styles.container}>
+      <View style={styles.activityContainer}>
         <Image style={styles.logo} source={logo} />
       </View>
     );
   };
+
   return (
     <WebView
       startInLoadingState
       renderLoading={() => {
         return <Loading />;
       }}
+      //style={styles.container}
       {...props}
     />
   );
 };
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'flex-start',
-    alignItems: 'center',
-  },
   logo: {
-    width: 50,
-    height: 50,
-    marginTop: -50,
+    width: 100,
+    height: 100,
+    marginTop: -100,
+  },
+
+  activityContainer: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    backgroundColor: '#FFF',
+    height: '100%',
+    width: '100%',
   },
 });
 export default WebViewScreen;
